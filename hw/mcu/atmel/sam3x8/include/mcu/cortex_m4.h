7052735267cb6b90fb0feac80719ffcede0bd32e
Joshua Anderson <j@joshua-anderson.com>
2017-11-12 21:51:27 -0800

    Start adding system HAL

---
+++ hw/mcu/atmel/sam3x8/include/mcu/cortex_m4.h
@@ -0,0 +1,9 @@
+#ifndef __MCU_CORTEX_M4_H__
+#define __MCU_CORTEX_M4_H__
+
+#include "../../src/sam/utils/cmsis/samd3x/include/sam3xa.h"
+
+/* TODO: this is from the M0, is this still true */
+#define OS_TICKS_PER_SEC (1000)
+
+#endif
---
+++ hw/mcu/atmel/sam3x8/src/hal_system.c
@@ -0,0 +1,37 @@
+#include <mcu/cortex_m0.h>
+#include "hal/hal_system.h"
+#include <stdlib.h>
+
+void hal_system_reset(void)
+{
+    while (1) {
+        if (hal_debugger_connected()) {
+            /*
+             * If debugger is attached, breakpoint here.
+             */
+            __asm__("bkpt");
+        }
+
+           /* TODO: from the M0, investigate for M4 */
+           /* Cortex-M0+ Core Debug Registers (DCB registers, SHCSR, and DFSR)
+           are only accessible over DAP and not via processor. Therefore
+           they are not covered by the Cortex-M0 header file. */
+
+        NVIC_SystemReset();
+    }
+}
+
+int hal_debugger_connected(void)
+{
+    return DSU->STATUSB.reg & DSU_STATUSB_DBGPRES;
+}
+
+uint32_t HAL_GetTick(void)
+{
+    return 0;
+}
+
+int HAL_InitTick (uint32_t TickPriority)
+{
+    return 0;
+}
