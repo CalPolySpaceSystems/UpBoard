ccd15ea194beb2edfb5a41e65c6ea07afaa01e65
pchizek <patrick.chizek@comcast.net>
2017-12-15 00:53:50 -0500

    Begin integrating twi methods in hal

--- hw/mcu/atmel/sam3x8/src/hal_i2c.c
+++ hw/mcu/atmel/sam3x8/src/hal_i2c.c
@@ -24,22 +24,45 @@
 
 int hal_i2c_init(uint8_t i2c_num, void *usercfg)
 {
-	// twi_master_init
+
+	uint8_t rc; // return code
+
+	// define twi options struct based on usercfg
+	struct twi_options {
+		//! MCK for TWI.
+		uint32_t master_clk = usercfg->master_clk;
+		//! The baud rate of the TWI bus.
+		uint32_t speed = usercfg->baud;
+		//! The desired address.
+		uint8_t chip = usercfg->chip_addr;
+		//! SMBUS mode (set 1 to use SMBUS quick command, otherwise don't).
+		uint8_t smbus = 0;
+	} twi_options_t;
+	
+	// Now that settings are in place, can initialize through twi
+	twi_master_init(/*twi instance?*/, twi_options_t);
+
+	return rc;
 }
 
 int hal_i2c_master_write(uint8_t i2c_num, struct hal_i2c_master_data *data,
 	uint32_t timo, uint8_t last_op)
 {
-	// master write
+	uint8_t rc; // return code
+	return rc;
 }
 
 int hal_i2c_master_read(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
 	uint32_t timo, uint8_t last_op)
 {
 	// master read
+	uint8_t rc; // return code
+	return rc;
 }
 
 int hal_i2c_master_probe(uint8_t i2c_num, uint8_t address, uint32_t timo)
 {
 	// owo what's this?
+	uint8_t rc; // return code
+	return rc;
 }
