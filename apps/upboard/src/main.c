5ec1dc773c8ce85ed5ec0f3a9042ca7268256d10
Joshua Anderson <j@joshua-anderson.com>
2017-12-14 18:51:35 -0800

    Add main file with console and pin toggling

--- apps/upboard/src/main.c
+++ apps/upboard/src/main.c
@@ -1,66 +1,75 @@
+#include <assert.h>
+#include <string.h>
+
+#include "sysinit/sysinit.h"
+#include "os/os.h"
+#include "bsp/bsp.h"
+#include "hal/hal_gpio.h"
+#ifdef ARCH_sim
+#include "mcu/mcu_sim.h"
+#endif
+
+static volatile int g_task1_loops;
+
+/* For LED toggling */
+int g_led_pin;
+
+/* The timer callout */
+static struct os_callout blinky_callout;
+
 /*
- * CPSS UpBoard X Main code
- *
- *
- *
+ * Event callback function for timer events. It toggles the led pin.
  */
+static void
+timer_ev_cb(struct os_event *ev)
+{
+    assert(ev != NULL);
 
-// Include necessary mynewt os + hal libraries
-//#include <os/os.h>
-//#include <bsp/bsp.h>
+    ++g_task1_loops;
+    hal_gpio_toggle(g_led_pin);
 
-//hardware abstraction layer (HAL)
-// this contains things like uart/twi drivers
-
-//#include <assert.h>
-//#include <sysinit/sysinit.h>
-
-// Include our sensor/functionality libraries (location subject to change)
-/*
-#include <module/a3g4250d.h>
-#include <module/lsm9ds1.h>
-#include <module/ms56xx.h>
-#include <module/ublox_gps>
-#include <module/xbee_spi>
-*/
-
-// define task priorities and stack sizes
-/*
-#define GEN_TASK_PRIO		3
-#define GEN_TASK_STACK_SZ	512
-*/
-// Pin and other definitions
-
-// variables
-
-// main function to run once
-int  main(int argc, char **argv) {
-
-	// initialize system
-	sysinit();
-
-	// initialize scheduled tasks (see os.h??)
-	//init_tasks();
-
-	while (1) {
-
-	}
-	assert(0);
-
+    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC/10);
 }
 
-/* initialize tasks 
-void init_tasks(void) {
+static void
+init_timer(void)
+{
+    /*
+     * Initialize the callout for a timer event.
+     */
+    os_callout_init(&blinky_callout, os_eventq_dflt_get(),
+                    timer_ev_cb, NULL);
 
+    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC/10);
 }
 
+/**
+ * main
+ *
+ * The main task for the project. This function initializes packages,
+ * and then blinks the BSP LED in a loop.
+ *
+ * @return int NOTE: this function should never return!
+ */
+int main(int argc, char **argv)
+{
+    int rc;
 
+#ifdef ARCH_sim
+    mcu_sim_parse_args(argc, argv);
+#endif
 
+    sysinit();
 
-/* event callbacks*/
+    g_led_pin = 21;
+    hal_gpio_init_out(g_led_pin, 1);
 
-// sensor reads
+    init_timer();
+    while (1) {
+        os_eventq_run(os_eventq_dflt_get());
+    }
+    assert(0);
 
-// packet transmissions
+    return rc;
+}
 
-// other
