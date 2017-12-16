294ac1a4d0bae6b8d7564e79f1fbe7875b5d3036
Joshua Anderson <j@joshua-anderson.com>
2017-12-15 18:39:06 -0800

    Add empty target to test compiling

---
+++ apps/empty/src/main.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <string.h>
+
+#include "sysinit/sysinit.h"
+#ifdef ARCH_sim
+#include "mcu/mcu_sim.h"
+#endif
+
+/**
+ * main
+ *
+ * This application does nothing, it's primary goal is to test compiling with the sam3x8 mcu
+ *
+ * @return int NOTE: this function should never return!
+ */
+int main(int argc, char **argv)
+{
+    int rc;
+
+#ifdef ARCH_sim
+    mcu_sim_parse_args(argc, argv);
+#endif
+
+    sysinit();
+    assert(0);
+
+    return rc;
+}
+
