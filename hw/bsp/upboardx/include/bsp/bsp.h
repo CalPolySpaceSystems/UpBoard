7f83db8128990d2c67d42300838b61247f2dfbde
Joshua Anderson <j@joshua-anderson.com>
2017-12-15 18:54:35 -0800

    Finish minimal BSP files

---
+++ hw/bsp/upboardx/include/bsp/bsp.h
@@ -0,0 +1,22 @@
+#ifndef __UPBOARD_X_BSP_H__
+#define __UPBOARD_X_BSP_H__
+
+#include <inttypes.h>
+#include <mcu/mcu.h>
+
+#include <syscfg/syscfg.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern uint8_t _ram_start;
+// TODO: This can be expanded if necessary
+#define RAM_SIZE        0x00008000
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
