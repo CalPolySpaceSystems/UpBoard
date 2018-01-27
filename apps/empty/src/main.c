9fc0b31989777a51e8f6adc7057e3fcdb7c6a437
Alex Gravenor <blazingkin@gmail.com>
2018-01-27 12:02:01 -0800

    f to pay respects

--- apps/empty/src/main.c
+++ apps/empty/src/main.c
@@ -4,6 +4,8 @@
 #include <hal/hal_gpio.h>
 #include <sysinit/sysinit.h>
 #include <os/os.h>
+#include <hal/hal_uart.h>
+
 
 #ifdef ARCH_sim
 #include "mcu/mcu_sim.h"
@@ -21,6 +23,11 @@
 volatile int led_dir;
 volatile int loops;
 
+
+int tx_funct(void *garbage){
+    return (int) 'h';
+}
+
 int main(int argc, char **argv)
 {
     int rc;
@@ -34,16 +41,19 @@ int main(int argc, char **argv)
     hal_gpio_init_out(LED_BLINK_PIN, 1);
     led_dir = 1;
     loops = 0;
-
+    #define USART_USING 1
+    hal_uart_init(USART_USING, NULL);
+    hal_uart_config(USART_USING, 9600, 8, 2, HAL_UART_PARITY_NONE, HAL_UART_FLOW_CTL_NONE);
+    hal_uart_init_cbs(USART_USING, &tx_funct, NULL, NULL, NULL);
     while(1) {
-        loops++;
+        hal_uart_start_tx(USART_USING);
+        /*loops++;
         os_time_delay(OS_TICKS_PER_SEC);
         led_dir = !led_dir;
-        hal_gpio_write(LED_BLINK_PIN, led_dir);
+        hal_gpio_write(LED_BLINK_PIN, led_dir);*/
     }
 
     assert(0);
 
     return rc;
 }
-
