b44e36ba3a5013379afa1240f1817f6fe09f2bb2
pchizek <patrick.chizek@comcast.net>
2018-02-24 11:19:49 -0800

    Used MN example of blinky with tasks.

--- apps/empty/src/main.c
+++ apps/empty/src/main.c
@@ -12,44 +12,53 @@
 
 #define LED_BLINK_PIN (59)
 
-/**
- * main
- *
- * This application does nothing, it's primary goal is to test compiling with the sam3x8 mcu
- *
- * @return int NOTE: this function should never return!
- */
-volatile int led_dir;
-volatile int loops;
-volatile int test;
+/* Define task stack and task object */
+#define MY_TASK_PRI         (OS_TASK_PRI_HIGHEST) 
+#define MY_STACK_SIZE       (64) 
+struct os_task my_task; 
+os_stack_t my_task_stack[MY_STACK_SIZE]; 
 
-int main(int argc, char **argv)
+/* This is the task function */
+void my_task_func(void *arg) {
+    /* Set the led pin as an output */
+    hal_gpio_init_out(LED_BLINK_PIN, 1);
+
+    /* The task is a forever loop that does not return */
+    while (1) {
+        /* Wait one second */ 
+        os_time_delay(1000);
+
+        /* Toggle the LED */ 
+        hal_gpio_write(LED_BLINK_PIN, 0);
+
+        /* Wait one second */ 
+        os_time_delay(1000);
+
+        /* Toggle the LED */ 
+        hal_gpio_write(LED_BLINK_PIN, 1);
+    }
+}
+
+/* This is the main function for the project */
+int main(int argc, char **argv) 
 {
-    int rc;
 
-#ifdef ARCH_sim
-    mcu_sim_parse_args(argc, argv);
-#endif
-
+    /* Perform system and package initialization */
     hal_bsp_init();
     sysinit();
 
-    hal_gpio_init_out(LED_BLINK_PIN, 1);
-    led_dir = 1;
-    loops = 0;
+    __asm__("bkpt");
 
+    /* Initialize the task */
+    int rc = os_task_init(&my_task, "my_task", my_task_func, NULL, MY_TASK_PRI, 
+                 OS_WAIT_FOREVER, my_task_stack, MY_STACK_SIZE);
 
-    while(1) {
-        //__asm__("bkpt");
-        test = os_cputime_get32();
-        loops++;
-        //os_time_delay(OS_TICKS_PER_SEC);
-        os_cputime_delay_ticks(1000000);
-        led_dir = !led_dir;
-        hal_gpio_write(LED_BLINK_PIN, led_dir);
+    assert(rc == OS_OK);
+    __asm__("bkpt");
+
+    /*  Process events from the default event queue.  */
+    while (1) {
+       os_eventq_run(os_eventq_dflt_get());
     }
-
-    assert(0);
-
-    return rc;
+    /* main never returns */  
 }
