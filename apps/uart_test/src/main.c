b57f8d83ad76739b9ae94aba39c9cd9e9e7f05f9
Alex Gravenor <blazingkin@gmail.com>
2018-03-03 12:50:13 -0800

    UART test

--- apps/uart_test/src/main.c
+++ apps/uart_test/src/main.c
@@ -1,55 +1,83 @@
 #include <assert.h>
 #include <string.h>
-#include <hal/hal_bsp.h>
-#include <hal/hal_gpio.h>
-#include <sysinit/sysinit.h>
-#include <os/os.h>
-#include <hal/hal_uart.h>
 
+#include "sysinit/sysinit.h"
+#include "os/os.h"
+#include "bsp/bsp.h"
+#include "hal/hal_gpio.h"
+#include "hal/hal_uart.h"
 #ifdef ARCH_sim
 #include "mcu/mcu_sim.h"
 #endif
 
-#define LED_BLINK_PIN (59)
+static volatile int g_task1_loops;
+
+/* For LED toggling */
+int g_led_pin;
+
+/* The timer callout */
+static struct os_callout blinky_callout;
+
+/*
+ * Event callback function for timer events. It toggles the led pin.
+ */
+static void
+timer_ev_cb(struct os_event *ev)
+{
+    assert(ev != NULL);
+
+    ++g_task1_loops;
+    hal_gpio_toggle(g_led_pin);
+
+    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC/10);
+}
+
+static void
+init_timer(void)
+{
+    /*
+     * Initialize the callout for a timer event.
+     */
+    os_callout_init(&blinky_callout, os_eventq_dflt_get(),
+                    timer_ev_cb, NULL);
+
+    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC/10);
+}
 
 /**
  * main
  *
- * This application does nothing, it's primary goal is to test compiling with the sam3x8 mcu
+ * The main task for the project. This function initializes packages,
+ * and then blinks the BSP LED in a loop.
  *
  * @return int NOTE: this function should never return!
  */
-volatile int led_dir;
-volatile int loops;
-volatile int test;
-
 int main(int argc, char **argv)
 {
-    int rc;
+    int rc = 0;
+    sysinit();
+    init_timer();
+    /*board_init();*/
+    /*sysclk_init();*/
+    usart_test();
+    
 
+    /*
 #ifdef ARCH_sim
     mcu_sim_parse_args(argc, argv);
 #endif
 
-    hal_bsp_init();
     sysinit();
 
-    hal_gpio_init_out(LED_BLINK_PIN, 1);
-    led_dir = 1;
-    loops = 0;
+    g_led_pin = 21;
+    hal_gpio_init_out(g_led_pin, 1);
 
-
-    while(1) {
-        //__asm__("bkpt");
-        test = os_cputime_get32();
-        loops++;
-        //os_time_delay(OS_TICKS_PER_SEC);
-        os_cputime_delay_ticks(1000000);
-        led_dir = !led_dir;
-        hal_gpio_write(LED_BLINK_PIN, led_dir);
+    init_timer();
+    while (1) {
+        os_eventq_run(os_eventq_dflt_get());
     }
-
     assert(0);
-
+    */
     return rc;
 }
+
