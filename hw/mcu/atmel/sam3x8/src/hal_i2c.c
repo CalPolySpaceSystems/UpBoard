03b6fbdece7a235e3832a2d0af4b1b1386822119
pchizek <patrick.chizek@comcast.net>
2018-03-10 13:24:36 -0800

    Old work on i2c implementation

--- hw/mcu/atmel/sam3x8/src/hal_i2c.c
+++ hw/mcu/atmel/sam3x8/src/hal_i2c.c
@@ -15,41 +15,51 @@
 #include <syscfg/syscfg.h>
 
 // hal
-#include <hal/hal_i2c.h>
-#include <hal/hal_gpio.h>
+#include "hal/hal_i2c.h"
+#include "hal/hal_gpio.h"
 
 // platform specific
-#include <sam3x8e.h> //???
+#include <sam3x8e.h>
 #include <twi.h>
 
+// mcu
+#include "mcu/hal_i2c.h"
+
+#define i2c_0 1;
+#define i2c_1 2;
 
 int hal_i2c_init(uint8_t i2c_num, void *usercfg)
 {
 
     uint8_t rc; // return code
 
-    // define twi options struct based on usercfg
-    struct twi_options {
-        //! MCK for TWI.
-        uint32_t master_clk = usercfg->master_clk;
-        //! The baud rate of the TWI bus.
-        uint32_t speed = usercfg->baud;
-        //! The desired address.
-        uint8_t chip = usercfg->chip_addr;
-        //! SMBUS mode (set 1 to use SMBUS quick command, otherwise don't).
-        uint8_t smbus = 0;
-    } twi_options_t;
-
+	assert(usercfg != NULL);
+	
     // Now that settings are in place, can initialize through twi
-    twi_master_init(/*twi instance?*/, twi_options_t);
-
+	if (i2c_num = i2c_0){
+		rc = twi_master_init(TWI0, &usercfg);
+	else{
+		return -1;
+	}
+	
     return rc;
 }
 
-int hal_i2c_master_write(uint8_t i2c_num, struct hal_i2c_master_data *data,
+int hal_i2c_master_write(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
     uint32_t timo, uint8_t last_op)
 {
     uint8_t rc; // return code
+	struct twi_packet pkt;
+	
+	// update the twi_packet struct
+	pkt.address = pdata->address;
+    pkt.data_length = pdata->len;
+    pkt.data = pdata->buffer;
+	
+	// actually write the byte, referencing TWI0 and the pkt
+	
+    rc = twi_master_write(TWI0, &pkt);
+   
     return rc;
 }
 
@@ -58,6 +68,16 @@ int hal_i2c_master_read(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
 {
     // master read
     uint8_t rc; // return code
+	struct twi_packet pkt;
+	
+	// update the twi_packet struct
+	pkt.addr = pdata->address;
+    pkt.addr_length = pdata->len;
+    pkt.data = pdata->buffer;
+	
+	// actually read the byte, referencing TWI0 and the pkt
+    rc = twi_master_read(TWI0, &pkt);
+   
     return rc;
 }
 
@@ -65,6 +85,10 @@ int hal_i2c_master_probe(uint8_t i2c_num, uint8_t address, uint32_t timo)
 {
     // owo what's this?
     uint8_t rc; // return code
+	
+	twi_probe();
+	
     return rc;
 }
+
 # endif
